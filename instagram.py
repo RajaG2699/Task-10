3c04b7eb86e1ecbce2a04eb388fffb1cb456c9f0
RajaG2699 <160389339+RajaG2699@users.noreply.github.com>
2024-07-04 19:08:43 +0530

    Add files via upload

---
+++ instagram.py
@@ -0,0 +1,53 @@
+# import os
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# # from webdriver_manager.chrome import ChromeDriverManager
+#
+# driver_path = r"C:\Users\Raja\Desktop\chromedriver.exe"
+# os.environ["PATH"] += os.pathsep + os.path.dirname(driver_path)
+#
+# chrome_options = Options()
+#
+# chrome_options.add_experimental_option("detach", True)
+# driver = webdriver.Chrome(options=chrome_options)
+# # Initialize Chrome driver (you may need to adjust this based on your setup)
+# # options = webdriver.ChromeOptions()
+# # options.add_argument("--headless")  # Run in headless mode, remove for visual testing
+# # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+driver_path = r"C:\Users\Raja\Desktop\chromedriver.exe"
+os.environ["PATH"] += os.pathsep + os.path.dirname(driver_path)
+
+chrome_options = Options()
+
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+# Open Instagram profile page
+driver.get("https://www.instagram.com/guviofficial/")
+
+try:
+    # Wait for the page to load (adjust the time as needed)
+    driver.implicitly_wait(10)
+
+    # Extract follower count
+    followers_element = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/div[3]/ul/li[2]/div/button/span/span')
+    followers_count = followers_element.text
+    print("Followers:", followers_count)
+
+    # Extract following count
+    following_element = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/div[3]/ul/li[3]/div/button/span/span')
+    following_count = following_element.text
+    print("Following:", following_count)
+
+finally:
+    # Close the browser
+    driver.quit()
